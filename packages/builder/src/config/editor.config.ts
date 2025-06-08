06f0dd88d24b4d7308ab821fbeda52090268d78e
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-06-08 15:20:38 +0600

    refactor: media control component

--- packages/builder/src/config/editor.config.ts
+++ packages/builder/src/config/editor.config.ts
@@ -1,6 +1,7 @@
 import { ButtonConfig } from "@/blocks/button";
 import { ContainerConfig } from "@/blocks/container";
 import { HeadingConfig } from "@/blocks/heading";
+import { ImageConfig } from "@/blocks/image";
 import { LinkConfig } from "@/blocks/link";
 import { TabsConfig } from "@/blocks/tabs";
 import { TextConfig } from "@/blocks/text";
@@ -12,5 +13,6 @@ export const BlockConfiguration = registerEditorBlocks([
   TextConfig,
   ButtonConfig,
   LinkConfig,
+  ImageConfig,
   TabsConfig,
 ]);
